86a0fbf56ac25a566e57c4e5d9ddcc9dd71ef588
trimstray <trimstray@gmail.com>
2018-05-28 09:03:55 +0200

    minor fixes in setup.sh - signed-off-by: trimstray <trimstray@gmail.com>

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ _arg="$1"
 
 if [[ "$1" == "install" ]] ; then
 
-  printf "%s\n" "Create symbolic link to /usr/local/bin"
+  printf "%s\\n" "Create symbolic link to /usr/local/bin"
 
   if [[ -e "${_dir}/bin/sandmap" ]] ; then
 
@@ -19,7 +19,7 @@ if [[ "$1" == "install" ]] ; then
 
   fi
 
-  printf "%s\n" "Create man page to /usr/local/man/man8"
+  printf "%s\\n" "Create man page to /usr/local/man/man8"
 
   if [[ -e "${_dir}/doc/man8/sandmap.8" ]] ; then
 
@@ -35,7 +35,7 @@ if [[ "$1" == "install" ]] ; then
 
 elif [[ "$1" == "uninstall" ]] ; then
 
-  printf "%s\n" "Remove symbolic link from /usr/local/bin"
+  printf "%s\\n" "Remove symbolic link from /usr/local/bin"
 
   if [[ -L "/usr/local/bin/sandmap" ]] ; then
 
@@ -43,7 +43,7 @@ elif [[ "$1" == "uninstall" ]] ; then
 
   fi
 
-  printf "%s\n" "Remove man page from /usr/local/man/man8"
+  printf "%s\\n" "Remove man page from /usr/local/man/man8"
 
   if [[ -e "/usr/local/man/man8/sandmap.8.gz" ]] ; then
 
